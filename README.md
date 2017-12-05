2ba672b428fc3aed2302d16f0865e38587ef4b3b
Dalton Carr <daltonlcarr@yahoo.com>
2017-12-05 15:23:39 -0800

    done

--- README.md
+++ README.md
@@ -1,31 +1,9 @@
-![cf](https://i.imgur.com/7v5ASc8.png) Lab 5: Linked List
-======
+Exported Module (Linked List):
 
-## Submission Instructions
-* Work in a fork of this repository
-* Work in a branch on your fork
-* Write all of your code in a directory named `lab-` + `<your name>` **e.g.** `lab-susan`
-* Open a pull request to this repository
-* Submit on canvas a question and observation, how long you spent, and a link to your pull request
+In the linked-list.js module I am exporting the LinkedList class. The LinkedList class has a constructor with an arity of one (value) and properties: this.value = value; and this.next = null;
 
-## Configuration 
-Configure the root of your repository with the following files and directories. Thoughtfully name and organize any additional configuration or module files.
-* **README.md** - contains documentation
-* **.gitignore** - contains a [robust](http://gitignore.io) `.gitignore` file 
-* **.eslintrc.json** - contains the course linter configuration
-* **.eslintignore** - contains the course linter ignore configuration
-* **package.json** - contains npm package config
-* **model/** - contains module definitions
-* **\_\_test\_\_/** - contains test modules
+Functions (append, remove, find):
 
-## Feature Tasks  
-#### Doubly Linked List
-* Given the LinkedList class presented during lecture, add a`find(value)` method that returns a node (if present) that corresponds to a particular value.
-  * You can assume that the linked list won't have duplicate values
+The append and remove functions each have the parameter 'node'. These two functions are working to either add a new node in to the linked list or to remove one.
 
-## Tests
-Unit test each method of your class. 
-
-## Documentation
-In your README.md describe the exported values of each module you have defined. Every function description should include it's arity (expected number of parameters), the expected data for each parameter (data-type and limitations), and it's behavior (for both valid and invalid use). Feel free to write any additional information in your README.md.
-
+The find function has the parameter 'value'. This function locates a value and returns it.
--- __test__/linked-list.test.js
+++ __test__/linked-list.test.js
@@ -4,43 +4,43 @@ const LinkedList = require('../model/linked-list')
 
 
 describe('linked-list-model.js',() => {
-    test('A list with a single element, should have a value and no next', () => {
-      let result = new LinkedList(100);
-      expect(result.value).toEqual(100);
-      expect(result.next).toEqual(null);
-    });
+  test('A list with a single element, should have a value and no next', () => {
+    let result = new LinkedList(100);
+    expect(result.value).toEqual(100);
+    expect(result.next).toEqual(null);
+  });
 
-    test('insertion should properly modify the next property', () => {
-        let result = new LinkedList(10);
-        result.append(new LinkedList(20));
-        result.append(new LinkedList(30));
+  test('insertion should properly modify the next property', () => {
+    let result = new LinkedList(10);
+    result.append(new LinkedList(20));
+    result.append(new LinkedList(30));
     
-        expect(result.value).toEqual(10);
-        expect(result.next.value).toEqual(20);
-        expect(result.next.next.value).toEqual(30);
-        expect(result.next.next.next).toEqual(null);
-      });
+    expect(result.value).toEqual(10);
+    expect(result.next.value).toEqual(20);
+    expect(result.next.next.value).toEqual(30);
+    expect(result.next.next.next).toEqual(null);
+  });
 
-      test('remove should update the next property and erase an element', () => {
-        let first = new LinkedList(10);
-        let second = new LinkedList(20);
-        let third = new LinkedList(30);
+  test('remove should update the next property and erase an element', () => {
+    let first = new LinkedList(10);
+    let second = new LinkedList(20);
+    let third = new LinkedList(30);
     
-        first.append(second);
-        first.append(third);
+    first.append(second);
+    first.append(third);
     
-        expect(first.value).toEqual(10);
-        expect(first.next.value).toEqual(20);
-        expect(first.next.next.value).toEqual(30);
-        expect(first.next.next.next).toEqual(null);
+    expect(first.value).toEqual(10);
+    expect(first.next.value).toEqual(20);
+    expect(first.next.next.value).toEqual(30);
+    expect(first.next.next.next).toEqual(null);
     
-        first.remove(second);
+    first.remove(second);
     
-        expect(first.value).toEqual(10);
-        expect(first.next.value).toEqual(30);
-        expect(first.next.next).toEqual(null);
+    expect(first.value).toEqual(10);
+    expect(first.next.value).toEqual(30);
+    expect(first.next.next).toEqual(null);
     
-      });
+  });
 
 });
 
