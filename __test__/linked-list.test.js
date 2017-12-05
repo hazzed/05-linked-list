732c1d0330b14e4fbf5aa3e57125f200b82675f4
Dalton Carr <daltonlcarr@yahoo.com>
2017-12-05 15:14:32 -0800

    2nd test done

--- __test__/linked-list.test.js
+++ __test__/linked-list.test.js
@@ -9,4 +9,17 @@ describe('linked-list-model.js',() => {
       expect(result.value).toEqual(100);
       expect(result.next).toEqual(null);
     });
+
+    test('insertion should properly modify the next property', () => {
+        let result = new LinkedList(10);
+        result.append(new LinkedList(20));
+        result.append(new LinkedList(30));
+    
+        expect(result.value).toEqual(10);
+        expect(result.next.value).toEqual(20);
+        expect(result.next.next.value).toEqual(30);
+        expect(result.next.next.next).toEqual(null);
+      });
+
 });
+
